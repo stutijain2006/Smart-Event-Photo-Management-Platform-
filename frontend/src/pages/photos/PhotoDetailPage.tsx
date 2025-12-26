5b192a37a4affaffcb6303b01279734ba17d09a8
Stuti Jain <cherrykj2019@gmail.com>
2025-12-26 20:15:39 +0530

    Added download, like, metadata form in the photoDetailPage

--- frontend/src/pages/photos/PhotoDetailPage.tsx
+++ frontend/src/pages/photos/PhotoDetailPage.tsx
@@ -2,21 +2,33 @@ import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import api from "../../services/api";
 import DashboardLayout from "../../components/dashboard/DashboardLayout";
+import CommentsModal from "../../components/photos/CommentsModal";
+import DownloadPhotoModal from "../../components/photos/DownloadPhotoModal";
+import MetadataModal from "../../components/photos/MetadataModal";
+import ShareModal from "../../components/photos/ShareModal";
+import Modal from "../../components/common/Modal";
 
 export default function PhotoDetailPage(){
-    const { photoId } = useParams();
+    const params = useParams();
+    const photoId = params.photoId;
     const navigate = useNavigate();
     const [photo, setPhoto] = useState<any>(null);
     const [showShare, setShowShare] = useState(false);
     const [showDetails, setShowDetails] = useState(false);
     const [showDownload, setShowDownload] = useState(false);
     const [showComments, setShowComments] = useState(false);
-    const [metadata, setMetadata] = useState<any>(null);
     const [comments, setComments] = useState<any[]>([]);
 
+    const loadData= async() => {
+        const photos= await api.get(`/photos/`);
+        setPhoto(photos.data.find((p: any) => p.photo_id === photoId));
+
+        const c = await api.get(`/photos/${photoId}/comments`);
+        setComments(c.data);
+    };
+
     useEffect(() => {
-        api.get(`/photos/`).then(res => setPhoto(res.data.find((photo: any) => photo.photo_id === photoId)));
-        api.get(`/photos/${photoId}/comments`).then(res => setComments(res.data));
+        loadData();
     }, [photoId]);
 
     const likePhoto = async() => {
@@ -34,7 +46,7 @@ export default function PhotoDetailPage(){
         window.open(photo.file_path_original, "_blank");
     };
 
-    if (!photo) return (<div>Loading...</div>);
+    if (!photoId) return (<div>Loading...</div>);
 
     return (
         <DashboardLayout>
@@ -45,16 +57,22 @@ export default function PhotoDetailPage(){
                 </div>
                 <img src= {photo.file_path_original} alt= "photo" className="w-[70vw] h-[70vh] object-contain rounded-lg" />
                 <div className="flex items-start justify-around px-4"> 
-                    <button onClick={sharePhoto} className="bg-gray-300 p-2 rounded-lg"> Share </button>
+                    <button onClick={() => setShowShare(true)} className="bg-gray-300 p-2 rounded-lg"> Share </button>
                     <div className="flex justify-center items-center gap-3">
                         <button onClick={likePhoto} className="bg-gray-300 p-2 rounded-lg"> ❤️ Like ({photo.like_count})</button>
-                        <button onClick={moreDetails} className="bg-gray-300 p-2 rounded-lg"> 👀 Details</button>
-                        <button onClick={downloadPhoto} className="bg-gray-300 p-2 rounded-lg"> ⬇ Download ({photo.download_count})</button>
+                        <button onClick={() => setShowDetails(true)} className="bg-gray-300 p-2 rounded-lg"> 👀 Details</button>
+                        <button onClick={() => setShowDownload(true)} className="bg-gray-300 p-2 rounded-lg"> ⬇ Download ({photo.download_count})</button>
                     </div>
-                    <div className="bg-gray-300 p-2 rounded-lg" > Comments</div>
+                    <div className="bg-gray-300 p-2 rounded-lg" onClick={() => setShowComments(true)} > Comments</div>
                 </div>
             </div>
+
+            <Modal isOpen={showShare} onClose={() => setShowShare(false)}><ShareModal photoId={photoId} /></Modal>
+            <Modal isOpen={showDetails} onClose={() => setShowDetails(false)}><MetadataModal metadata={photo.photo_metadata} /></Modal>
+            <Modal isOpen={showDownload} onClose={() => setShowDownload(false)}><DownloadPhotoModal onDownload = {downloadPhoto} /></Modal>
+            <Modal isOpen={showComments} onClose={() => setShowComments(false)}><CommentsModal 
+            photoId={photoId} refresh={loadData} comments={comments} /></Modal>
         </DashboardLayout>
-    )
+    );
 }
 
